6aa6b17f7a2715f6929e51a3abfbe1bc1bd7acf8
Kristian <84454508+SlorRy@users.noreply.github.com>
2024-08-15 23:07:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const SteamUser = require('steam-user');
+
+const user = new SteamUser();
+
+const logOnOptions = {
+	accountName: '',  //Enter here your account login
+	password: ''		 //Enter your password
+}
+
+user.logOn(logOnOptions);
+
+user.on('loggedOn', () => {
+	console.log(logOnOptions.accountName + ' - Successfully logged on');
+	user.setPersona(1);                 //1 - online, 7 - invisible
+	user.gamesPlayed([730,570,252490,271590,359550,1172620,1517290,2221490]);    //List app IDs
+});
